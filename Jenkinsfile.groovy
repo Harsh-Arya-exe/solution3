fdb042bd712e6206aafb39e91803b4a083eb94c3
Harsh-Arya-exe <harsh.arya1004@gmail.com>
2024-10-16 21:52:39 +0530

    jenkinsfile

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,16 @@
+pipeline{
+    agent any
+    stages{
+        stage('Build){
+            echo 'Building...'
+            echo 'python3 P1.py'
+        }
+        stage('Test'){
+            echo 'Testing...'
+        }
+        stage('Deploy){
+            echo 'Deploy'
+        }
+
+    }
+}
